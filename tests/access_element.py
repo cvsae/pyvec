8349a9b63d8a770c2e10313f3f81c3e03a2f8d4f
cvsae <cvsc@gmx.com>
2019-07-26 18:10:01 +0300

    The first commit

---
+++ tests/access_element.py
@@ -0,0 +1,41 @@
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+
+from pyvec import *
+
+
+
+
+def main():
+    v1 = Vector()
+
+    n = 5
+    f = [5, 9, 67, 65, 99]
+
+    for x in xrange(0, int(n)):
+        v1.push_back(f[x])
+
+
+
+
+    # ================== Show value at position 2 using Reference Operator ===============
+    print "\nReference operator v1[2]= %s " %v1[2]
+
+    # ================== Show value at position 3 using at() =======================
+    # @ not implemented yet
+
+    # ===================Show the first element of the vector=============================
+    print "\nFirst element of the vector is: %s" %v1.front()
+
+    #================Show the last element of the vector==================================
+    print "\nLast element of the vector is: %s " %v1.back() 
+    
+    #================Showing all the elements  using data=================================
+    # @ not implemented yet
+
+
+
+
+main() 
---
+++ tests/iterator.py
@@ -0,0 +1,38 @@
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+
+from pyvec import *
+
+
+
+
+def main():
+    v1 = Vector()
+
+    n = 5
+    f = [5, 9, 67, 65, 99]
+
+    for x in xrange(0, int(n)):
+        v1.push_back(f[x])
+
+
+    # ================Print all element using begin() & end()==========================
+    v = v1.begin()
+    while v != v1.end():
+        print v[0]
+        v +=1
+
+
+    # =====================Print all element using cbegin() & cend()======================//
+    # @ not implemented yet
+
+    # ====================Print all element using crbegin() & crend()===================//
+    # @ not implemented yet
+
+    # =====================Print all element using rbegin() & rend()==========================//
+    # @ not implemented yet
+   
+
+main()

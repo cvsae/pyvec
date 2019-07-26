53263859f38fc55f4e389fda09a2e6e9fdf8c8d6
cvsae <cvsc@gmx.com>
2019-07-26 18:14:00 +0300

    changes

--- tests/access_element.py
+++ tests/access_element.py
@@ -3,7 +3,7 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 
-from pyvec import *
+from vector import *
 
 
 
--- tests/iterator.py
+++ tests/iterator.py
@@ -3,7 +3,7 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 
-from pyvec import *
+from vector import *
 
 
 
@@ -33,6 +33,6 @@ def main():
 
     # =====================Print all element using rbegin() & rend()==========================//
     # @ not implemented yet
-   
+
 
 main()

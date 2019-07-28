e929d5b3f5c5049d3f1e982de3bb8766fb8cf6f3
cvsae <33999023+cvsae@users.noreply.github.com>
2019-07-28 17:49:07 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,5 +14,6 @@ setup(
     'Development Status :: 3 - Alpha',      
     'Topic :: Software Development :: Build Tools',    'License :: OSI Approved :: MIT License',   
     'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.4',
   ],
 )

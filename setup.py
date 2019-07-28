7a53b4241233cb99f33aa136d8d0626a03bb59e1
cvsae <33999023+cvsae@users.noreply.github.com>
2019-07-28 17:49:36 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'pyvec',         
   py_modules=["iters", "vector"],  
-  version = '0.2',     
+  version = '0.3',     
   license='MIT',       
   description = 'Poor c++ vector implementation for python',   
   author = 'cvsae',                  
   author_email = 'cvsc@gmx.com',     
   url = 'https://github.com/cvsae/pyvec',   
-  download_url = 'https://github.com/cvsae/pyvec/archive/v_02.tar.gz',
+  download_url = 'https://github.com/cvsae/pyvec/archive/v_03.tar.gz',
   keywords = ['vector', 'python', 'c++', "implementation"],   
   classifiers=[
     'Development Status :: 3 - Alpha',      
